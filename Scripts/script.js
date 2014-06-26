faf45c1a3b5b16e811190c18be1c1842533c5840
bsef10m046 <bsef10m046@pucit.edu.pk>
2014-06-27 03:06:46 +0500

    new

---
+++ Scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".list-services .tooltips").easyTooltip();
+});
+$(window).load(function () {
+    $('#bgSlider').nivoSlider({
+        duration: 1000,
+        pagination: '.pagination',
+        preload: true,
+        slideshow: 30000,
+        spinner: '.bg_spinner'
+    });
+});
